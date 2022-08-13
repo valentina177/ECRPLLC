a9a289a67604031f493bfa3cd7c6118b52fe4a4d
ValentinaGL <60409104+valentina177@users.noreply.github.com>
2022-08-13 02:53:55 -0400

    Add files via upload

--- apartments/ap7/gallery.js
+++ apartments/ap7/gallery.js
@@ -7,11 +7,7 @@ imgArray = [
   ['images/3.png'],
   ['images/4.png'],
   ['images/5.png'],
-  ['images/6.png'],
-  ['images/7.png'],
-  ['images/8.png'],
-  ['images/9.png'],
-  ['images/10.png'],
+  
 ]
 
 //Function called by clicking on 'next' link
