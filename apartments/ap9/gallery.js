67941d480b331721cf58b6ebe895ef2e3359cd37
ValentinaGL <60409104+valentina177@users.noreply.github.com>
2022-08-13 02:54:50 -0400

    Add files via upload

---
+++ apartments/ap9/gallery.js
@@ -0,0 +1,47 @@
+var num = 0;
+
+//Replace with your own values and add additional lines for more images
+imgArray = [
+  ['images/1.png'],
+  ['images/2.png'],
+  ['images/3.png'],
+  ['images/4.png'],
+  ['images/5.png'],
+  ['images/6.png'],
+  ['images/7.png'],
+  ['images/8.png'],
+  ['images/9.png'],
+  ['images/10.png'],
+  ['images/11.png'],
+  ['images/12.png'],
+  ['images/13.png'],
+  ['images/14.png'],
+  ['images/15.png'],
+  ['images/16.png'],
+  ['images/17.png'],
+  ['images/18.png'],
+  ['images/19.png'],
+  ['images/20.png'],
+  ['images/21.png'],
+  ['images/22.png'],
+  ['images/23.png'],
+  ['images/24.png'],
+  ['images/25.png'],
+
+]
+
+//Function called by clicking on 'next' link
+//Replace id name with the id name you gave to the img
+function slideshowUp() {
+  num++;
+  num = num % imgArray.length;
+  document.getElementById('slide_show').src=imgArray[num];
+}
+
+//Function called by clicking on 'previous' link
+function slideshowBack() {
+  num--;
+  if (num < 0) {num=imgArray.length-1;}
+  num = num % imgArray.length;
+  document.getElementById('slide_show').src=imgArray[num];
+}
