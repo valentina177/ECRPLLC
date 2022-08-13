5df82276001410859956bb0eca50c05c311c2385
ValentinaGL <60409104+valentina177@users.noreply.github.com>
2022-08-13 02:50:20 -0400

    Add files via upload

--- apartments/ap2/gallery.js
+++ apartments/ap2/gallery.js
@@ -10,14 +10,7 @@ imgArray = [
   ['images/6.png'],
   ['images/7.png'],
   ['images/8.png'],
-  ['images/9.png'],
-  ['images/10.png'],
-  ['images/11.png'],
-  ['images/12.png'],
-  ['images/13.png'],
-  ['images/14.png'],
-  ['images/15.png'],
-  ['images/16.png'],
+  
 
 ]
 
